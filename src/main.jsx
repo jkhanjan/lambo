48bffb4813dbf8d125349dd95a4b098329a2da07
jkhanjan <jhakhanjan62@gmail.com>
2026-03-22 12:49:06 +0530

    build failed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { CarProvider } from "./components/context/carContext";
+import { CarProvider } from "./components/context/CarContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
