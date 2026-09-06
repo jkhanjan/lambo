25e121fbfd5799e31eb006bc6feeecd0bd4f1a71
jkhanjan <jhakhanjan62@gmail.com>
2026-09-06 23:54:47 +0530

    camera changes

--- src/components/utils/ResponsiveCamera.jsx
+++ src/components/utils/ResponsiveCamera.jsx
@@ -12,7 +12,6 @@ const ResponsiveCamera = () => {
       camera.position.set(0, 5, 20);
       camera.fov = 45;
     } else {
-      // camera.position.set(0, 5, 1);
       camera.fov = 55;
     }
     camera.updateProjectionMatrix();
